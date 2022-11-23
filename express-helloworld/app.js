cbbc42938bfc510da16d264e18055ea7455747b8
Student User <student@workstation.lab.example.com>
2022-11-23 05:44:45 -0500

    update version 2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,14 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  response = "This is version 2 of the app" + "\n";
+
+  //send response to client
+  res.send(response);
+});
+
+app.get('/mars', function(req, res) {
+    res.send('Hello Mars!\n');
 });
 
 app.listen(8080, function () {
